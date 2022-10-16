ea0361415e83c759ffc9699842793a88e43b1c7c
paramee@bk.ru <paramee@bk.ru>
2022-10-16 23:49:18 +0400

    fixed containers

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,45 +6,50 @@ const Skills = () => {
   return (
     <div className="px-4 py-8 bg-red-300 text-center">
       <ul className="md:flex md:justify-around">
-        <li className="p-8">
-          <p className="text-xl font-bold">Video Production</p>
-          <ul>
-            <li className="py-2">
-              <p data-tip="Indepented">Adobe Premiere Pro</p>
+        <li className="px-8">
+          <p className="text-xl font-bold pb-4">Video Production</p>
+          <ul className="pb-2 w-40 md:w-80 m-auto">
+            <li>
+              <p className="pb-2" data-tip="Indepented">Adobe Premiere Pro</p>
               <ReactTooltip />
             </li>
-            <li className="py-2">
-              <p className="" data-tip="Intermediate">Adobe After Effects</p>
+            <li className="pb-2">
+              <p className="" data-tip="Intermediate">
+                Adobe After Effects
+              </p>
               <ReactTooltip />
             </li>
-            <li className="py-2">
-              <p data-tip="Intermediate">Adobe Audition</p>
+            <li>
+              <p className="pb-2 sd:w-1/4" data-tip="Intermediate">Adobe Audition</p>
               <ReactTooltip />
             </li>
-            <li className="py-2">
+            <li className="pb-4">
               <p data-tip="Basic">Davinci Resolve</p>
               <ReactTooltip />
             </li>
           </ul>
         </li>
-        <li className="p-8">
-          <p className="text-xl font-bold">Web Development</p>
+        <li className="px-8">
+          <p className="text-xl font-bold pb-4">Web Development</p>
           <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Tailwind</li>
-            <li>Git</li>
-            <li>Node JS</li>
+            <li className="pb-2">JavaScript</li>
+            <li className="pb-2">React</li>
+            <li className="pb-2">Tailwind</li>
+            <li className="pb-2">Git</li>
+            <li className="pb-4">Node JS</li>
           </ul>
         </li>
-        <li className="p-8">
-          <p className="text-xl font-bold">Other</p>
+        <li className="px-8">
+          <p className="text-xl font-bold pb-4">Other</p>
           <ul>
-            <li title="B1">English</li>
-            <li title="Native">Russian</li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li className="pb-2">
+              <p data-tip="B1">English</p>
+              <ReactTooltip />
+            </li>
+            <li className="pb-4">
+              <p data-tip="Native">Russian</p>
+              <ReactTooltip />
+            </li>
           </ul>
         </li>
       </ul>
