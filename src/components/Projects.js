d2bbced1b0e91c92ec23b407fb95f3a983c88cbd
paramee@bk.ru <paramee@bk.ru>
2022-10-19 13:58:02 +0400

    revome modal-video

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,12 @@
 import HoverVideoPlayer from "react-hover-video-player";
 import testJpg from "../assets/test_1x1.jpg";
 import testMov from "../assets/test.MOV";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Projects = () => {
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 max-w-ld p-8 bg-red-300">
+    <div className="grid grid-cols-1 max-w-screen-md md:grid-cols-2 md:max-w-screen-2xl m-auto bg-red-300">
       <HoverVideoPlayer
         videoSrc={testMov}
         pausedOverlay={
