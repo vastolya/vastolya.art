33f0ea04e7a2ac5ce37da660e7e95896bb2f032e
paramee@bk.ru <paramee@bk.ru>
2022-10-22 18:10:37 +0400

    react-youtube component added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,27 +4,36 @@ import testMov from "../assets/test.MOV";
 import React, { useState, useEffect, useCallback, Fragment } from "react";
 import { CenterModal, ModalTitle, ModalCloseTarget } from "react-spring-modal";
 import "react-spring-modal/styles.css";
+import YouTube from "react-youtube";
 
 const PROJECTS_LIST = [
   {
     id: "first",
     pausedOverlay: testJpg,
     videoSrc: testMov,
+    videoId: "2g811Eo7K8U",
+    title: "first video title",
   },
   {
     id: "second",
     pausedOverlay: testJpg,
     videoSrc: testMov,
+    videoId: "ryPBF-jy9Es",
+    title: "second video title",
   },
   {
     id: "third",
     pausedOverlay: testJpg,
     videoSrc: testMov,
+    videoId: "SWE2Hww3_AM",
+    title: "third video title",
   },
   {
     id: "fourth",
     pausedOverlay: testJpg,
     videoSrc: testMov,
+    videoId: "KGIOVaRCtxA",
+    title: "fourth video title",
   },
 ];
 
@@ -40,12 +49,30 @@ const Projects = () => {
     setSelected(undefined);
   };
 
+  const onPlayerReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts = {
+    height: "480",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      playing: true,
+      autoplay: 1,    
+    },
+  };
+
   return (
     <div className="grid gap-0 grid-cols-1 max-w-screen-md md:grid-cols-2 md:max-w-screen-2xl mx-auto bg-red-300">
       {PROJECTS_LIST.map((project) => {
         return (
           <Fragment key={project.id}>
-            <button className="leading-disable" onClick={() => onSelected(project.id)}>
+            <button
+              className="leading-disable"
+              onClick={() => onSelected(project.id)}
+            >
               <HoverVideoPlayer
                 videoSrc={project.videoSrc}
                 pausedOverlay={
@@ -57,8 +84,6 @@ const Projects = () => {
                       width: "100%",
                       height: "100%",
                       objectFit: "cover",
-                      paddingBottom: 0,
-                                            
                     }}
                   />
                 }
@@ -73,9 +98,16 @@ const Projects = () => {
               isOpen={project.id === selected}
               onDismiss={handleClose}
             >
-              <ModalTitle>{project.id}</ModalTitle>
+              <ModalTitle>
+                <YouTube
+                  videoId={project.videoId}
+                  opts={opts}
+                  onReady={onPlayerReady}
+                />
+              </ModalTitle>
               <ModalCloseTarget>
-                <button>Close</button>
+                <div className="text-xl py-8">{project.title}</div>
+                <button className="text-sm underline">close</button>
               </ModalCloseTarget>
             </CenterModal>
           </Fragment>
