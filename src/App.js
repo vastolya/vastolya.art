536b68702c20dc6abe3ac71d0b32d5073d4d8c5f
paramee@bk.ru <paramee@bk.ru>
2022-10-23 17:20:10 +0400

    react-router-dom is working now

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Education from "./components/Education";
 import Header from "./components/Header";
@@ -7,14 +8,31 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <div className="container__layout">
-      <Header />
-      <Projects />
-      <Main />
-      <Skills />
+    // <div className="container__layout">
+    //   <Header />
+    //   <Projects />
+    //   <Main />
+    //   <Skills />
 
-      <Education />
+    //   <Education />
+    // </div>
+
+    <>
+    <div className="bg-slate-200 flex justify-around h-12">
+      <div className="m-3">vastolya.art</div>
+      <ul className="flex gap-x-12 m-3">
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/skills">Skills</Link></li>
+        <li><Link to="/education">Education</Link></li>
+      </ul>
     </div>
+        <Routes>
+        <Route index element={<Main />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="education" element={<Education />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from 'react-dom/client';
-import 'react-spring-modal/styles.css';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "react-spring-modal/styles.css";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+  <BrowserRouter>
+  <App />
+</BrowserRouter>,
+ );
