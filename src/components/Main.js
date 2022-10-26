06e8b5ec16f5c0dd27182e9c9e91bfdc2f709e5d
paramee@bk.ru <paramee@bk.ru>
2022-10-26 23:22:12 +0400

    6 total project-items, render shuffle

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,21 @@
 import HoverVideoPlayer from "react-hover-video-player";
-import artRomaJpg from "../assets/art_zakladky_roma_soida.jpg"
-import artRomaMp4 from "../assets/art_zakladky_roma_soida.mp4"
-import artAlexeyJpg from "../assets/art_zakladky_alexey_martins.jpg"
-import artAlexeyMp4 from "../assets/art_zakladky_alexey_martins.mp4"
-import artBlosyakJpg from "../assets/art_zakladky_sasha_blosyak.jpg"
-import artBlosyakMp4 from "../assets/art_zakladky_sasha_blosyak.mp4"
-import artZakirovJpg from "../assets/art_zakladky_sanya_zakirov.jpg"
-import artZakirovMp4 from "../assets/art_zakladky_sanya_zakirov.mp4"
+import artRomaJpg from "../assets/art_zakladky_roma_soida.jpg";
+import artRomaMp4 from "../assets/art_zakladky_roma_soida.mp4";
+import artAlexeyJpg from "../assets/art_zakladky_alexey_martins.jpg";
+import artAlexeyMp4 from "../assets/art_zakladky_alexey_martins.mp4";
+import artBlosyakJpg from "../assets/art_zakladky_sasha_blosyak.jpg";
+import artBlosyakMp4 from "../assets/art_zakladky_sasha_blosyak.mp4";
+import artZakirovJpg from "../assets/art_zakladky_sanya_zakirov.jpg";
+import artZakirovMp4 from "../assets/art_zakladky_sanya_zakirov.mp4";
+import artAzbuniakJpg from "../assets/art_zakladky_azbuniak.jpg";
+import artAzbuniakMp4 from "../assets/art_zakladky_azbuniak.mp4";
+import artLukeJpg from "../assets/art_zakladky_vadim_luke.jpg";
+import artLukeMp4 from "../assets/art_zakladky_vadim_luke.mp4";
 
 
 
 import React, { useState, useEffect, useCallback, Fragment } from "react";
+import shuffle from "lodash/shuffle";
 // import { CenterModal, ModalTitle, ModalCloseTarget } from "react-spring-modal";
 // import "react-spring-modal/styles.css";
 
@@ -21,6 +26,7 @@ const PROJECTS_LIST = [
     videoSrc: artZakirovMp4,
     videoId: "d2gQYhUiu20",
     title: "Art Zakladky - Sanya Zakirov",
+    releaseDate: "29.11.2020"
   },
   {
     id: "second",
@@ -28,6 +34,7 @@ const PROJECTS_LIST = [
     videoSrc: artBlosyakMp4,
     videoId: "d6O03m1Toe4",
     title: "Art Zakladky - Sasha Blosyak",
+    releaseDate: "21.08.2020"
   },
   {
     id: "third",
@@ -35,6 +42,7 @@ const PROJECTS_LIST = [
     videoSrc: artAlexeyMp4,
     videoId: "KHB-V0fQw9k",
     title: "Art Zakladky - Alexey Martins",
+    releaseDate: "30.01.2022"
   },
   {
     id: "fourth",
@@ -42,7 +50,24 @@ const PROJECTS_LIST = [
     videoSrc: artRomaMp4,
     videoId: "KHB-V0fQw9k",
     title: "Art Zakladky - Roma Soida",
+    releaseDate: "03.01.2021"
   },
+  {
+    id: "fith",
+    pausedOverlay: artAzbuniakJpg,
+    videoSrc: artAzbuniakMp4,
+    videoId: "Gdrd5Ub51EA",
+    title: "Art Zakladky - Azbuniak",
+    releaseDate: "22.05.2020"
+  },
+  {
+    id: "sixth",
+    pausedOverlay: artLukeJpg,
+    videoSrc: artLukeMp4,
+    videoId: "9bxevb7J_sA",
+    title: "Art Zakladky - Vadim Luke",
+    releaseDate: "30.06.2020"
+  }
 ];
 
 const Projects = () => {
@@ -72,36 +97,38 @@ const Projects = () => {
 
   return (
     <div className="grid gap-0 grid-cols-1 w-full md:grid-cols-2 md:max-w-screen-2xl mx-auto">
-      {PROJECTS_LIST.map((project) => {
-        return (
-          <Fragment key={project.id}>
-            <button
-              className="leading-disable max-w-screen-2xl"
-              onClick={() => onSelected(project.id)}
-            >
-              <HoverVideoPlayer
-                className="z-0"
-                videoSrc={project.videoSrc}
-                pausedOverlay={
-                  <div
-                    alt=""
-                    style={{
-                      // Make the image expand to cover the video's dimensions
-                      width: "100%",
-                      height: "100%",
-                      backgroundImage: `url(${project.pausedOverlay})`,
-                      backgroundSize: "cover",
-                    }}
-                  />
-                }
-                loadingOverlay={
-                  <div className="loading-overlay">
-                    <div className="loading-spinner" />
-                  </div>
-                }
-              />
-            </button>
-            {/* <CenterModal
+      {shuffle(PROJECTS_LIST)
+        .slice(0, 4)
+        .map((project) => {
+          return (
+            <Fragment key={project.id}>
+              <button
+                className="leading-disable max-w-screen-2xl"
+                onClick={() => onSelected(project.id)}
+              >
+                <HoverVideoPlayer
+                  className="z-0"
+                  videoSrc={project.videoSrc}
+                  pausedOverlay={
+                    <div
+                      alt=""
+                      style={{
+                        // Make the image expand to cover the video's dimensions
+                        width: "100%",
+                        height: "100%",
+                        backgroundImage: `url(${project.pausedOverlay})`,
+                        backgroundSize: "cover",
+                      }}
+                    />
+                  }
+                  loadingOverlay={
+                    <div className="loading-overlay">
+                      <div className="loading-spinner" />
+                    </div>
+                  }
+                />
+              </button>
+              {/* <CenterModal
               isOpen={project.id === selected}
               onDismiss={handleClose}
             >
@@ -122,9 +149,9 @@ const Projects = () => {
                 <button className="text-sm underline">close</button>
               </ModalCloseTarget>
             </CenterModal> */}
-          </Fragment>
-        );
-      })}
+            </Fragment>
+          );
+        })}
     </div>
   );
 };
