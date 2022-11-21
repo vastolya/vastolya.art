3e1ff2d5bf18f1a42bdc437f737edfff8ea5c02a
paramee@bk.ru <paramee@bk.ru>
2022-11-21 13:54:30 +0700

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import About from "./components/About";
-import CV from "./components/CV";
+// import CV from "./components/CV";
 import ProjectsPage from "./pages/ProjectsPage";
 import SingleProjectPage from "./pages/SingleProjectPage";
 
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route index element={<Main />} />
         <Route path="about" element={<About />} />
-        <Route path="cv" element={<CV />} />
+        {/* <Route path="cv" element={<CV />} /> */}
         <Route path="projects" element={<ProjectsPage />} />
         <Route path="projects/:projectId" element={<SingleProjectPage />} />
       </Routes>
